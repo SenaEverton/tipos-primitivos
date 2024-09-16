484beb7b3f51d4b7eb22eaac379f171eb72fb627
SenaEverton <tom.sena@gmail.com>
2024-09-16 22:40:19 +0000

    atualizando código

--- aula1.js
+++ aula1.js
@@ -1,1 +1,17 @@
 
+ola = "Hello Worl"
+console.log(ola)
+
+
+
+rotulo = "natalya"
+console.log (rotulo)
+
+rotulo = 12345
+console.log(rotulo)
+
+
+const num1 = 10
+const num2 = 20
+const calc = num1 - num2
+console.log("O resultado é " + calc)
